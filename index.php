37d3ca98714a35e35c7057f1371371d190a03915
Dandy Ojeda <dandyojeda@gmail.com>
2023-07-25 18:01:10 +0800

    added sort options

--- index.php
+++ index.php
@@ -6,6 +6,41 @@ require_once('banner.php');
 
 <section class="py-5">
     <div class="container-xl">
+        <div class="row mb-5">
+            <div class="col-md-6">
+                <ul class="nav nav-pills nav-fill nav-filter">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Sweaters</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Tops</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Jackets</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Hats</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-6 d-flex justify-content-end align-items-end flex-column filter-sort-wrap">
+                <div class="d-flex align-items-center filter-sort">
+                    <span class="me-3">Sort By</span>
+                    <div class="dropdown">
+                        <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Popular
+                        </button>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div>Showing 1003 Products</div>
+            </div>
+        </div>
+
         <div class="row">
             <?php
             // echo "home";
